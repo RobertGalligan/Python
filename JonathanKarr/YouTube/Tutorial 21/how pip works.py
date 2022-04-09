96f5ed28839c76bf59c479a46e92b7a9f021e191
Robert Galligan <90750996+RobertGalligan@users.noreply.github.com>
2022-04-09 15:46:02 -0400

    PCEP
    COMMIT

---
+++ JonathanKarr/YouTube/Tutorial 21/how pip works.py
@@ -0,0 +1,7 @@
+# pip --version
+# python3.9 -m pip install --upgrade pip
+# pip install numpy
+# python3.9 -m pip install numpy
+import numpy as np
+# all 0s matrix
+print(np.zeros((2,2), dtype = "int32"))
