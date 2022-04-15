987c240d6fa0ff061620102d6754b56ecfaa75c9
Robert Galligan <robert_galligan@hotmail.com>
2022-04-15 14:15:56 -0400

    changes

---
+++ Scripts/Variables and Built-in Functions part 1.py
@@ -0,0 +1,21 @@
+a = 5                               #Variables can be changed to any value at any time.
+aNumber = 10
+A_Really_Big_Number = 100000000
+b = 5.5
+c = 0.001
+print(type(a))
+print(type(b))
+print(type(c))
+txt = "Hello"
+d = "Margie"
+print(type(txt))
+print(type(d))
+e = True
+print(type(e))
+print(a)
+a = 100
+print(a)
+a = "Tree"
+print(a)
+a= 4.4
+a = True
