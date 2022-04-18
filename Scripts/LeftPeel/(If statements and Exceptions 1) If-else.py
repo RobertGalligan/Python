168b826596386de1c33f51865719b7f97eeb7ed0
Robert Galligan <robert_galligan@hotmail.com>
2022-04-17 20:35:04 -0400

    updatre

---
+++ Scripts/LeftPeel/(If statements and Exceptions 1) If-else.py
@@ -0,0 +1,43 @@
+tim_age = 65
+tim_sex = "M"
+
+if tim_age < 20 and tim_sex == "M":
+    print("Man, your young!")
+elif tim_age < 40:
+    print("Getting older every day")
+elif tim_age < 55:
+    print("Almost an old man eh?")
+elif tim_age == 56:
+    print("That's my age!")
+else:
+    print("You're an old fart!")
+
+tim_age = 15
+tim_sex = "F "
+
+if tim_age < 20 and tim_sex == "M":
+    print("Man, your young!")
+elif tim_age < 40:
+    print("Getting older every day")
+elif tim_age < 55:
+    print("Almost an old man eh?")
+elif tim_age == 56:
+    print("That's my age!")
+else:
+    print("You're an old fart!")
+     
+
+tim_age = 15
+tim_sex = "M"
+
+if tim_age < 20 and tim_sex == "M":
+    print("Man, your young!")
+elif tim_age > 30 and tim_age < 40:
+    print("Getting older every day")
+elif tim_age < 55:
+    print("Almost an old man eh?")
+elif tim_age == 56:
+    print("That's my age!")
+else:
+    print("You're an old fart!")
+     
