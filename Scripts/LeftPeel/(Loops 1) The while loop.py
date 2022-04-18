ce5268f3717914ff606c017fd63a283669dbd545
Robert Galligan <robert_galligan@hotmail.com>
2022-04-18 16:06:30 -0400

    update

--- Scripts/LeftPeel/(Loops 1) The while loop.py
+++ Scripts/LeftPeel/(Loops 1) The while loop.py
@@ -2,9 +2,16 @@
 
 count = 1
 
-while count is < 5:
+while count <= 5:
     print(count)
     count = count + 1
 
 print("End of loop!")
 
+count = 10
+
+while count >= 0:
+    print(count)
+    count = count - 1
+
+print("End of the world!")
